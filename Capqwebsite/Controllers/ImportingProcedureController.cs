83ae2fc13c085bef4ed3b1599d344c4377d25dd5
Rehab-Hosny <Rehab-Hosny@DESKTOP-F38CV4D>
2025-09-22 11:08:53 +0300

    rehab

--- Capqwebsite/Controllers/ImportingProcedureController.cs
+++ Capqwebsite/Controllers/ImportingProcedureController.cs
@@ -82,8 +82,5 @@ namespace Capqwebsite.Controllers
                 return View();
             }
         }
-       
-
-        
     }
 }
---
+++ ViewModels/ExportingProcedureVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class ExportingProcedureVM
+    {
+        public long IDInitiator { get; set; }
+        public string ?CountryName { get; set; }
+        public string ?itemName { get; set; }
+        public string? ShortName_Ar { get; set; }
+        public string? ConstrainText { get; set; }
+        public short? Country_Id { get; set; }
+        public long? Item_ShortName_ID { get; set; }
+        public long? Item_ID { get; set; }
+        public string? InSide_Certificate_Ar { get; set; }
+
+
+        public string? ConstrainText_Ar { get; set; }
+
+        public string? ConstrainText_En { get; set; }
+
+   
+
+        public string? InSide_Certificate_En { get; set; }
+
+    }
+}
