af47a755bffd880643f786f368afb3c38deddb3e
Rehab-Hosny <Rehab-Hosny@DESKTOP-F38CV4D>
2025-11-20 13:53:01 +0200

    R__20-11-2025

--- Capqwebsite/Controllers/OfficesController.cs
+++ Capqwebsite/Controllers/OfficesController.cs
@@ -10,8 +10,15 @@ namespace Capqwebsite.Controllers
         [Route("/Offices/Index")]
         public IActionResult Index()
         {
+            //AgricultureDBContext DBContext = new AgricultureDBContext();
+            //var list=DBContext.Outlets.Where (a=>a.IsActive==true).ToList();
+            //return View(list);
+
             AgricultureDBContext DBContext = new AgricultureDBContext();
-            var list=DBContext.Outlets.Where (a=>a.IsActive==true).ToList();
+            var list = DBContext.WebsiteTypeDetails.Where(a => a.WebsitetypeID == 12 && (a.IsActive == true || a.IsActive == null)).ToList();
+            string TypeAr = DBContext.Websitetypes.Where(a => a.ID == 12 && (a.IsActive == true || a.IsActive == null)).ToList().FirstOrDefault()?.TypeAr;
+
+            ViewBag.TypeAr = TypeAr;
             return View(list);
         }
     }
