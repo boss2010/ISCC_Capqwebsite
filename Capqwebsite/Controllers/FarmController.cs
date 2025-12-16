2a531718eb6e5776a3f157a6f14b3ffc4d6331c8
shahenda <shahenda@DESKTOP-4810O0M>
2025-12-16 14:28:36 +0300

    menna

---
+++ Capqwebsite/Controllers/FarmController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Capqwebsite.Controllers
+{
+    public class FarmController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
