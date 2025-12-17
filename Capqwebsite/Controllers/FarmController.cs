72594d7e85a047bcfdade2604706fe5c6d29ca60
boss2010 <53015924+boss2010@users.noreply.github.com>
2025-12-17 10:27:15 +0200

    sayed

--- Capqwebsite/Controllers/FarmController.cs
+++ Capqwebsite/Controllers/FarmController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EF.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ViewModels;
 
 namespace Capqwebsite.Controllers
 {
     public class FarmController : Controller
     {
-        public IActionResult Index()
+        [AllowAnonymous]
+        [Route("/Farm/Index")]
+        public IActionResult Index(string FarmCode)
         {
-            return View();
+            AgricultureDBContext dbContext = new AgricultureDBContext();
+
+            var FarmDate = dbContext.FarmStops.Where(a => a.Farmcode == FarmCode).Select(a=> new FarmStopVM
+            {
+                Id=a.Id,
+                StopDate = a.StopDate,
+                Farmcode = a.Farmcode,
+                Cropname = a.Cropname,
+                Previewdate = a.Previewdate,
+                Compname = a.Compname,
+                Farmname = a.Farmname,
+                Text104 = a.Text104,
+            }).ToList();
+
+
+            return View(FarmDate);
         }
     }
 }
---
+++ ViewModels/FarmStopVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class FarmStopVM
+    {
+        public long Id { get; set; }
+
+        public string? StopDate { get; set; }
+
+        public string? Farmcode { get; set; }
+
+        public string? Cropname { get; set; }
+
+        public string? Previewdate { get; set; }
+
+        public string? Compname { get; set; }
+
+        public string? Farmname { get; set; }
+
+        public string? Text104 { get; set; }
+    }
+}
