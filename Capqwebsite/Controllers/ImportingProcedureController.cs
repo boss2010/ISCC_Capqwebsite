7da3343a5821d048d9b7bacb94534afdc0e12e16
Rehab-Hosny <Rehab-Hosny@DESKTOP-F38CV4D>
2025-12-24 14:32:30 +0200

    rehab

--- Capqwebsite/Controllers/ImportingProcedureController.cs
+++ Capqwebsite/Controllers/ImportingProcedureController.cs
@@ -18,14 +18,18 @@ namespace Capqwebsite.Controllers
             //////////////////////list of Im_Initiators////////////////////////////////
             var DataIm_Initiator = (from im in dbContext.Im_Initiators
                                     join c in dbContext.Countries on im.Country_Id equals c.ID
-                                    where im.IsActive == true
+                                    where im.IsActive == true&&c.IsActive == true 
+                                    && c.User_Deletion_Date == null
+                               && c.User_Deletion_Id == null
+                               && im.User_Deletion_Date == null
+                               && im.User_Deletion_Id == null
                                     select new Im_InitiatorVM
                                     {
                                         IDInitiator = c.ID,
                                         InitiatorNameAr = c.Ar_Name,
                                         InitiatorNameEn = c.En_Name,
                                         //Country_Id = im.Country_Id,
-                                        //Item_ShortName_ID = im.Item_ShortName_ID,
+                                        Item_ShortName_ID = im.Item_ShortName_ID,
                                     }).Distinct().OrderBy(x => x.InitiatorNameAr).ToList();
 
 
@@ -33,22 +37,19 @@ namespace Capqwebsite.Controllers
             var culture = new CultureInfo("ar-SA");
             //////////////////////list of Im_Initiators////////////////////////////////
            
-            var DataItem = (from i in dbContext.Items
+            var DataItem = (from i in dbContext.Item_ShortNames
                             join Im_In in dbContext.Im_Initiators
-                            on i.ID equals Im_In.Item_ShortName.Item.ID
+                            on i.ID equals Im_In.Item_ShortName_ID
 
                             select new ItemVM
                             {
-                                ID = Im_In.Item_ShortName.Item.ID,
-                                Name_Ar = i.Name_Ar,
-                                Name_En = i.Name_En,
+                                ID = i.ID,
+                                Name_Ar = i.ShortName_Ar,
+                                Name_En = i.ShortName_En,
 
                                 //Country_Id = im.Country_Id,
                                 //Item_ShortName_ID = im.Item_ShortName_ID,
-                            })
-                                .Where(i => i.Name_Ar != null)
-
-    .Distinct().OrderBy(i => i.Name_Ar).ToList();
+                            }).Where(i => i.Name_Ar != null).Distinct().OrderBy(i => i.Name_Ar).ToList();
             ViewData["ItemList"] = new SelectList(DataItem, "ID", "Name_Ar");
             ////////////////////////////
 
@@ -56,7 +57,9 @@ namespace Capqwebsite.Controllers
             {
                 var List = (from Im_In in dbContext.Im_Initiators
                             join intext in dbContext.Im_Constrain_Initiator_Texts on Im_In.ID equals intext.Im_Initiator_ID
-                            where Im_In.Country_Id == ImInitiatorID && Im_In.Item_ShortName.Item.ID == ItemID && intext.IsActive==true
+                            where Im_In.Country_Id == ImInitiatorID 
+                            && Im_In.Item_ShortName_ID == ItemID 
+                            && intext.IsActive==true
                             select new Im_InitiatorVM
                             {
                                 IDInitiator = Im_In.ID,
